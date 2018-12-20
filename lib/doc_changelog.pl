03151f264aecfda557694b4a7937260de4b8d4d5
Yu <euro21st@gmail.com>
2018-12-21 08:39:15 +0900

    Add update information, Ver.1.0.1 .

--- lib/doc_changelog.pl
+++ lib/doc_changelog.pl
@@ -42,10 +42,10 @@ sub outhtmlnew {
 	my $atr_id = $sow->{'html'}->{'atr_id'};
 
 	print <<"_HTML_";
-<h3>Sower Version 1.0 (2014/04/23)</h3>
+<h3>Sower Version 1.0.1 (2018/12/21)</h3>
 <ul>
-  <li>発言数回復機能\をオミット。</li>
-  <li>バージョン表\記を変更。</li>
+  <li>もっと読むボタンの挙動がおかしかったのを修正。</li>
+  <li>新着発言取得ボタンの挙動がおかしかったのを修正。</li>
 </ul>
 _HTML_
 
@@ -68,6 +68,12 @@ _HTML_
 	$self->outhtmlnew();
 
 	print <<"_HTML_";
+<hr class="invisible_hr"$net>
+<h3>Sower Version 1.0 (2014/04/23)</h3>
+<ul>
+  <li>発言数回復機能\をオミット。</li>
+  <li>バージョン表\記を変更。</li>
+</ul>
 <hr class="invisible_hr"$net>
 <h3>Version 2.3.3EU (2011/01/18)</h3>
 <ul>
