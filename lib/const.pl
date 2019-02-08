68488d2a63983428d1ba112d9c23bdbfa4325a15
Yu <euro21st@gmail.com>
2019-02-09 06:29:05 +0900

    Version up -> 1.1.3.

--- lib/const.pl
+++ lib/const.pl
@@ -347,7 +347,7 @@ sub InitConst {
 		COPY_AUTHOR => '‚ä[‚ë',
 		URL_AUTHOR  => 'https://github.com/y-moriya/sower/',
 		SITE_AUTHOR => 'github/sower',
-		VERSION_SW  => 'sower ver. 1.1.2',
+		VERSION_SW  => 'sower ver. 1.1.3',
 
 		QUERY_INVALID    => \%queryinvalid,
 		QUERY_SHORT2FULL => \%query_short2full,
