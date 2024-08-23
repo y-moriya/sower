f1dd6543d4718fc3b6e4f8867df082fe90fb321c
Yu MORIYA <euro21st@gmail.com>
2024-08-23 12:13:07 +0000

    Fix bug

--- doc/sow.js
+++ doc/sow.js
@@ -539,7 +539,7 @@ $(document).ready(function () {
 		if (normalRadio) {
 			normalRadio.addEventListener('change', updateTextareaStyle);
 		}
-		else if (monologueRadio) {
+		if (monologueRadio) {
 			monologueRadio.addEventListener('change', updateTextareaStyle);
 		}
 
